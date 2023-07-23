eb7d93bfbdec8b681b9ac74ec2c935499a27f0a5
Sogo <sshiozawa@enthought.com>
2023-07-23 17:25:16 +0900

    add post users

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,11 @@ from typing import List
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
-import crud
-import models
-from database import SessionLocal, engine
-import schemas
+from backend import crud
+from backend import models
+from backend.database import SessionLocal, engine
+from backend import schemas
 
-# What is this for?
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -22,10 +21,19 @@ def get_db():
         db.close()
 
 
+@app.post("/users", response_model=schemas.User)
+def create_user(
+        user: schemas.UserCreate,
+        manager_id,
+        db: Session = Depends(get_db)
+):
+    crud.create_user(db, user, manager_id)
+
+
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 50, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
+    db_users = crud.get_users(db, skip=skip, limit=limit)
+    return db_users
 
 
 @app.get("/users/{user_id}", response_model=List[schemas.User])
