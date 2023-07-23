46b15fdb61a2f1239ed4cdb90ff462d543bd0e60
Sogo <sshiozawa@enthought.com>
2023-07-23 21:19:33 +0900

    update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,71 @@
-import React, {useState} from "react"
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
 
 import AppHeader from "./components/AppHeader";
 import SelectUserDialog from "./components/SelectUserDialog";
-import './App.css';
+import SelectRecognitionTypeDialog from "./components/SelectRecognitionTypeDialog";
+import RecognitionDetail from "./components/RecognitionDetailDialog";
+import "./App.css";
 import Toolbar from "@mui/material/Toolbar";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
-
-const fakeUsers = [
-  {username: "sogoogos"},
-  {username: "abdf"},
-  {username: "sshiozawa"},
-  {username: "sogoogos"},
-  {username: "abdf"},
-  {username: "sshiozawa"},
-  {username: "sogoogos"},
-  {username: "abdf"},
-  {username: "sshiozawa"},
-  {username: "sogoogos"},
-  {username: "abdf"},
-  {username: "sshiozawa"},
-]
+const fakeUsers = [...Array(10).keys()].map(i => ({
+  username: `user${i}`,
+  id: i
+}));
 
 function App() {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [openSelectUser, setOpenSelectUser] = useState(false);
+  const [openSelectType, setOpenSelectType] = useState(false);
+  const [openRecognitionDetail, setOpenRecognitionDetail] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [selectedType, setSelectedType] = useState(0);
+  const [recognitionBody, setRecognitionBody] = useState("");
+
+  const selectUser = user => {
+    setSelectedUser(user);
+    setOpenSelectUser(false);
+    setOpenSelectType(true);
+  };
+
+  const selectRecognitionType = type => {
+    setSelectedType(type);
+    setOpenSelectType(false);
+    setOpenRecognitionDetail(true);
+  };
 
   const loginUsername = "sogo";
   return (
     <div className="App">
-      <AppHeader username={loginUsername}/>
-      <Toolbar/>
-      <Typography>
-          Hello. You can recognize your colleagues here.
-      </Typography>
+      <AppHeader username={loginUsername} />
+      <Toolbar />
+      <Typography>Hello. You can recognize your colleagues here.</Typography>
       <Box maxwidth="sm">
-        <Button variant="contained" onClick={handleOpen}>Recognize</Button>
+        <Button variant="contained" onClick={() => setOpenSelectUser(true)}>
+          Recognize
+        </Button>
       </Box>
-      <SelectUserDialog open={open} onClose={handleClose} users={fakeUsers}/>
+      <SelectUserDialog
+        open={openSelectUser}
+        onClose={() => setOpenSelectUser(false)}
+        users={fakeUsers}
+        onClick={selectUser}
+      />
+      <SelectRecognitionTypeDialog
+        open={openSelectType}
+        onClose={() => setOpenSelectType(false)}
+        user={selectedUser}
+        onClick={selectRecognitionType}
+      />
+      <RecognitionDetail
+        open={openRecognitionDetail}
+        onClose={() => setOpenRecognitionDetail(false)}
+        user={selectedUser}
+        type={selectedType}
+        body={recognitionBody}
+        onChange={setRecognitionBody}
+      />
     </div>
   );
 }
